43ca8fd2333aefc04507705219bd195a9e0bf284
kristenmills <kristen@kristen-mills.com>
2014-01-27 10:25:28 -0500

    Actually send the grammar.

--- Music/MusicClient.cs
+++ Music/MusicClient.cs
@@ -1,6 +1,8 @@
 ﻿using Mycroft.App;
+using Mycroft.App.Message;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,28 +14,33 @@ namespace Music
         private Dictionary<string, string> stt;
         private Dictionary<string, string> speakers;
         private string status;
+        private bool sentGrammar;
+        private string grammar;
 
         public MusicClient() : base()
         {
             stt = new Dictionary<string, string>();
             speakers = new Dictionary<string, string>();
             status = "down";
+            sentGrammar = false;
+            var reader = new StreamReader("grammar.xml")
+            grammar = reader.ReadToEnd();
         }
 
         protected async override void Response(Mycroft.App.Message.APP_DEPENDENCY type, dynamic message)
         {
             if (message.ContainsKey("stt"))
             {
-                var new_stt = message["stt"];
-                foreach (var kv in new_stt)
+                var newStt = message["stt"];
+                foreach (var kv in newStt)
                 {
                     stt[kv.Key] = kv.Value;
                 }
             }
             if (message.ContainsKey("audioOutput"))
             {
-                var new_speakers = message["audioOutput"];
-                foreach (var kv in new_speakers)
+                var newSpeakers = message["audioOutput"];
+                foreach (var kv in newSpeakers)
                 {
                     speakers[kv.Key] = kv.Value;
                 }
@@ -41,10 +48,17 @@ namespace Music
             if (status == "down" && stt.ContainsKey("stt1") && stt["stt1"] == "up" && speakers.ContainsKey("speakers") && speakers["speakers"] == "up")
             {
                 await SendData("APP_UP", "");
+                if (!sentGrammar)
+                {
+                    await SendJson("MSG_QUERY", new MessageQuery("stt", "load_grammar", new { name = "music", xml = grammar }, new string[] { }, 30));
+                    sentGrammar = true;
+                }
             }
             else if (status == "up" && (stt["stt1"] == "down" || speakers["speakers"] == "down"))
             {
                 await SendData("APP_DOWN", "");
+                await SendJson("MSG_QUERY", new MessageQuery("stt", "unload_gramamr", new { grammar = "music" }, new string[] { }, 30));
+                sentGrammar = false;
             }
         }
 
