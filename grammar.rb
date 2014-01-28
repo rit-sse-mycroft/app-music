03bf439a5586175cf91a593f786d84f5e7f2a621
kristenmills <kristen@kristen-mills.com>
2014-01-28 12:09:41 -0500

    Updated the grammar

--- grammar.rb
+++ grammar.rb
@@ -5,7 +5,7 @@ module MusicGrammar
 
   extend self
 
-  grammar 'music' do
+  grammar 'topLevel' do
     private_rule 'type' do
       one_of do
         item 'artist'
@@ -15,13 +15,33 @@ module MusicGrammar
       end
     end
 
+    private_rule 'action' do
+      one_of do
+        item 'play'
+        item 'pause'
+      end
+    end
+
     private_rule 'music' do
-      item 'Mycroft play the'
+      item 'play the'
       reference 'type'
       tag 'out.type=rules.type;'
       reference 'grammar:dictation'
       tag 'out.media=rules.latest();'
       item 'on spotify', repeat: "0-1"
+    end
+
+    private_rule 'music_control' do
+      reference 'action'
+      tag 'out.action=rules.action'
+    end
+
+    private_rule 'topLevel' do
+      item 'Mycroft'
+      one_of do
+        reference_item 'music'
+        reference_item 'music_control'
+      end
     end
   end
 end
